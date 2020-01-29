4d8a4642502ec3657109d3d82971e1980d0e4583
Andrew Woodham <andrewwoodham3@gmail.com>
2020-01-28 19:01:52 -0600

    Added console logs to requests, ensuring that all of them function properly.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -93,12 +93,11 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
-function unisfunction(){
-if(universities.includes(`Uni`) >= true){
-  unisWithUni.push(universities);
-} else null;
-
-};
+for (let i = 0; i < universities.length; i++) {
+  if (universities[i].includes(`Uni`) ){
+    unisWithUni.push(universities[i]);
+  }
+}
 console.log(unisWithUni);
 
 
@@ -142,10 +141,12 @@ console.log(lowCaseAnimalNames);
 
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
-*/const popeval = (e) => e.population < 5;
+*/
+const popeval = (e) => e.population < 5;
 
 const lowPopulationAnimals = zooAnimals.filter(popeval);
 
+console.log(lowPopulationAnimals)
 
 
 
